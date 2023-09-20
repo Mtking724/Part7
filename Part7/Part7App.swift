cd125b301edfd25ee2247da4e250d69449fd23e2
motoki0724 <motokiokayasu@gmail.com>
2023-09-20 19:21:47 +0900

    Initial Commit

---
+++ Part7/Part7App.swift
@@ -0,0 +1,17 @@
+//
+//  Part7App.swift
+//  Part7
+//
+//  Created by Motoki Okayasu on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Part7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
