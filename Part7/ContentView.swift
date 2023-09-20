a4f3c71da3c00821c4aca45053e450c76f31e937
motoki0724 <motokiokayasu@gmail.com>
2023-09-20 19:43:07 +0900

    Add navigation stack

--- Part7/ContentView.swift
+++ Part7/ContentView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello,world!")
+                
+            }
+            .padding()
+            .navigationTitle("画面1")
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
